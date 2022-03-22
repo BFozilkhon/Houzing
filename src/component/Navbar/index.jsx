ace40c0b3c0d77ae0be7a720acb85735b1f84ae5
Fozilkhon <buzrukxojayevfozilxon2007@gmail.com>
2022-03-22 06:07:06 -0700

    route fixed

--- src/component/Navbar/index.jsx
+++ src/component/Navbar/index.jsx
@@ -1,27 +1,33 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import { data } from "../../utils/navbar";
 import { Wrap } from "./style";
 
 export const Navbar = () => {
   return (
-    <Wrap>
-      <Wrap.LogoWrap>
-        <Wrap.NavLink to="/home">
-          <Wrap.Logo />
-          <span>Houzing</span>
-        </Wrap.NavLink>
-      </Wrap.LogoWrap>
-      <Wrap.Ul>
-        {data.map(
-          (value) =>
-            !value?.hidden && (
-              <Wrap.Li key={value?.id}>
-                <Wrap.NavLink to={value?.pathname}>{value?.title}</Wrap.NavLink>
-              </Wrap.Li>
-            )
-        )}
-      </Wrap.Ul>
-    </Wrap>
+    <>
+      <Wrap>
+        <Wrap.LogoWrap>
+          <Wrap.NavLink to="/home">
+            <Wrap.Logo />
+            <span>Houzing</span>
+          </Wrap.NavLink>
+        </Wrap.LogoWrap>
+        <Wrap.Ul>
+          {data.map(
+            (value) =>
+              !value?.hidden && (
+                <Wrap.Li key={value?.id}>
+                  <Wrap.NavLink to={value?.pathname}>
+                    {value?.title}
+                  </Wrap.NavLink>
+                </Wrap.Li>
+              )
+          )}
+        </Wrap.Ul>
+      </Wrap>
+      <Outlet />
+    </>
   );
 };
 
---
+++ src/root/PrivateRoute.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const PrivateRoute = ({ children }) => {
+  return <div>Fozil</div>;
+};
+
+export default PrivateRoute;
