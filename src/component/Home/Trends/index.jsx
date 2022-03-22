943d7a728e6377c7f2cf665c1b0aee9937d0c13c
Fozilkhon <buzrukxojayevfozilxon2007@gmail.com>
2022-03-21 23:29:14 -0700

    trends done

---
+++ src/component/Home/Trends/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "./style";
+
+export const Trends = () => {
+  return (
+    <Container>
+      <Container.Title>TRENDS</Container.Title>
+      <Container.Header>
+        Vermont Farmhouse With Antique Jail Is the Week's Most Popular Home
+      </Container.Header>
+      <Container.Btn>Read More</Container.Btn>
+    </Container>
+  );
+};
+
+export default Trends;
